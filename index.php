923c8ffcf97c3d78a68322417351093caec5915a
Marine <marine@MacBook-Pro-de-Marine.local>
2017-03-07 11:25:02 +0100

    Modif majuscule Smarty dans index

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-require("lib/smarty/smarty.class.php"); // On inclut la classe Smarty
+require("lib/smarty/Smarty.class.php"); // On inclut la classe Smarty
 $smarty = new Smarty();
 $smarty->display("presentation.html");
 ?>
