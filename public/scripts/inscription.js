01f75f64b4dac4b5a9a46323b99754eeec4d8bd7
Justine <justylebv@hotmail.fr>
2017-04-09 21:11:43 +0200

    Formulaire : côté js ok

--- public/scripts/inscription.js
+++ public/scripts/inscription.js
@@ -1,25 +1,6 @@
 
-function checkMail(input) {
-    if (input.value != document.getElementById('emailAddr').value) {
-        input.setCustomValidity('Les deux adresses e-mail ne correspondent pas.');
-    } else {
-        // le champ est valide : on réinitialise le message d'erreur
-        input.setCustomValidity('');
-    }
-}
-
-
-function checkPhone(input) {
-
-    /*if (input.value != "0[0-9]{9}") {
-        input.setCustomValidity('Le format du numéro est incorrect');
-    } else {
-        // le champ est valide : on réinitialise le message d'erreur
-        input.setCustomValidity('');
-    }*/
-
-
-    var reg = new RegExp("^0[1-9]([-. ]?[0-9]{2}){4}$");
+function checkFNames(input) {
+    var reg = new RegExp("^([a-zA-Z])+([- ]?[a-zA-Z])+$");
 
     if(reg.test(input.value))
     {
@@ -28,40 +9,106 @@ function checkPhone(input) {
     }
     else
     {
-        input.setCustomValidity('Le format du numéro est incorrect');
+        input.setCustomValidity('Le champ doit commencer et finir par des lettres');
+    }
+}
+
+
+function dateNow(input) {
+    var vDate = new Date();
+    var mois = vDate.getUTCMonth()+1; // getUTCMonth commence à 0
+    if (mois < 10){
+        mois = '0' + mois; //sinon non reconnu dans le onclick
+    }
+
+    var day = vDate.getUTCDate();
+    if (day < 10){
+        day = '0' + day;
+    }
+
+    var maxDate= vDate.getUTCFullYear()+'-'+mois+'-'+day;
+    var minDate = vDate.getUTCFullYear()-150+'-'+mois+'-'+day;
+
+    //alert('vDate = '+vDate+'   vDateMax = '+maxDate+' minDate = '+minDate + '    mois = '+mois);
+    input.setAttribute("max", maxDate);
+    input.setAttribute("min", minDate);
+}
+
+
+function checkMail(input) {
+    if (input.value != document.getElementById('emailAddr').value) {
+        input.setCustomValidity('Les deux adresses e-mail doivent être identiques');
+    } else {
+        // le champ est valide : on réinitialise le message d'erreur
+        input.setCustomValidity('');
+    }
+}
+
+
+function checkPhone(input) {
+    var reg = new RegExp("^0[1-9]([-. ]?[0-9]{2}){4}$");
+    var message ='';
+    if(reg.test(input.value))
+    {
+        // le champ est valide : on réinitialise le message d'erreur
+        input.setCustomValidity('');
+    }
+    else
+    {
+        message = 'Le format du numéro est incorrect';
+        if(input.value.charAt(0) != 0 ) {
+            message = message+': il doit commencer par un 0';
+        }
+        else {
+            message = message + ': doit contenir 10 chiffres';
+        }
+        input.setCustomValidity(message);
+    }
+}
+
+
+function checkLogin(input) {
+    var reg = new RegExp("^[a-zA-Z1-9]+$");
+
+    if(reg.test(input.value))
+    {
+        // le champ est valide : on réinitialise le message d'erreur
+        input.setCustomValidity('');
+    }
+    else
+    {
+        input.setCustomValidity('Le login doit contenir des chiffres et/ou des lettres');
     }
 }
 
 
 function checkPassword(input) {
-    /*alert("premier mdp : " + input.value + "  -  " + input.innerText + " \n   " + document.getElementById('password').value + "   -  " + document.getElementById('password').innerText);
-    if (input.value != document.getElementById('password').value) {
-        input.setCustomValidity('Les deux mdp ne correspondent pas.');
-    } else {
+    var reg = new RegExp("^[\\S.]{6,20}$");
+
+    if(reg.test(input.value))
+    {
         // le champ est valide : on réinitialise le message d'erreur
         input.setCustomValidity('');
-    }*/
+    }
+    else
+    {
+        input.setCustomValidity('Le mot de passe doit contenir entre 6 et 20 caractères sans espace');
+    }
+}
 
-    var mdp = document.getElementById("password_main").value; //la virgule répercute le var sur la deuxième déclaration
+
+function checkPasswordRepeat(input) {
+    var mdp = document.getElementById("password_main").value;
     var mdp2 = document.getElementById("password_repeat").value; //donc pas besoin de var ici
     if (mdp != mdp2)
     {
         //alert("je compare : " + mdp + " avec " + mdp2);
-        input.setCustomValidity('Les deux mdp ne correspondent pas.');
+        input.setCustomValidity('Les deux mots de passe doivent être identiques');
     }
     else
     {
         input.setCustomValidity('');
     }
-
 }
 
 
-function checkPostDate(input){
-    if(document.getElementById('arrival_dt').valueAsNumber < input.valueAsNumber ){
-        input.setCustomValidity('');
-    }
-    else{
-        input.setCustomValidity('La date de départ n est pas postérieure a la date d arrivee');
-    }
-}
--- templates/presentation.tpl
+++ templates/presentation.tpl
@@ -50,17 +50,20 @@ The setting is quiet and inviting, allowing patients to relax and enjoy a little
     <ul>
         <li>
             <label for="name">Nom&nbsp;:</label>
-            <input type="text" id="name" name="name" placeholder="Dupont" required>
+            <input type="text" id="name" name="name" placeholder="Dupont" required
+                   oninput="checkFNames(this);">
         </li>
 
         <li>
             <label for="firstname">Prénom&nbsp;:</label>
-            <input type="text" id="firstname" name="firstname" placeholder="Camille" required>
+            <input type="text" id="firstname" name="firstname" placeholder="Camille" required
+                   oninput="checkFNames(this)">
         </li>
 
         <li>
             <label for="birthdate">Date de naissance&nbsp;:</label>
-            <input type="date" id="birthdate" name="birthdate" required>
+            <input type="date" id="birthdate" name="birthdate" max="" min="" required
+                onblur="dateNow(this)" onclick="dateNow(this)" >  {*onblur (quand on quitte le champ) pour la saisie manuelle, onclick pour le calendrier*}
         </li>
 
         <li>
@@ -77,24 +80,25 @@ The setting is quiet and inviting, allowing patients to relax and enjoy a little
         <li>
             <label for="phone">Numéro de téléphone&nbsp;:</label>
             <input type="text" id="phone" name="phone" placeholder="0123456789" required
-                   oninput="checkPhone(this);">
+                   oninput="checkPhone(this)">
         </li>
 
         <li>
             <label for="login">Login&nbsp;:</label>
-            <input type="text" id="login" name="login" required>
+            <input type="text" id="login" name="login" required
+                   oninput="checkLogin(this)">
         </li>
 
         <li>
             <label for="password">Mot de passe&nbsp;:</label>
             <input type="password" id="password_main" name="password_main"  required
-             oninput="checkPassword(this)">
+                   oninput="checkPassword(this)">
         </li>
 
         <li>
             <label for="password_addr_repeat">Confirmez votre mot de passe&nbsp;:</label>
             <input type="password" id="password_repeat" name="password_repeat" required
-            oninput="checkPassword(this)">
+                   oninput="checkPasswordRepeat(this)">
         </li>
 
         <li>
